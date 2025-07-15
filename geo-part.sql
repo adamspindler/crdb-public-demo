853e10d7fff9ae5109220d552be27ee38c2ed84c
Adam Spindler <spindler@cockroachlabs.com>
2025-07-15 12:20:01 +0100

    Altered the table region pinning

--- geo-part.sql
+++ geo-part.sql
@@ -10,15 +10,15 @@ alter table mr.eu_multi_b configure zone using num_replicas=3, gc.ttlseconds=900
 
 alter table mr.eu_m2m configure zone using num_replicas=3, gc.ttlseconds=90000, constraints = '[+region=EMEA]';
 
-alter table mr.us_parent configure zone using num_replicas=3, gc.ttlseconds=90000, constraints = '[-region=NAMR]';
+alter table mr.us_parent configure zone using num_replicas=3, gc.ttlseconds=90000, constraints = '[+region=NAMR]';
 
-alter table mr.us_child configure zone using num_replicas=3, gc.ttlseconds=90000, constraints = '[-region=NAMR]';
+alter table mr.us_child configure zone using num_replicas=3, gc.ttlseconds=90000, constraints = '[+region=NAMR]';
 
-alter table mr.us_multi_a configure zone using num_replicas=3, gc.ttlseconds=90000, constraints = '[-region=NAMR]';
+alter table mr.us_multi_a configure zone using num_replicas=3, gc.ttlseconds=90000, constraints = '[+region=NAMR]';
 
 alter table mr.us_multi_b configure zone using num_replicas = 4, constraints = '{"+region=NAMR": 2, "+region=APAC": 2}', num_voters=3, voter_constraints = '{"+region=NAMR": 1, "+region=APAC": 1}';
 
-alter table mr.us_m2m configure zone using num_replicas=3, gc.ttlseconds=90000, constraints = '[-region=NAMR]';
+alter table mr.us_m2m configure zone using num_replicas=3, gc.ttlseconds=90000, constraints = '[+region=NAMR]';
 
 ALTER TABLE mr.global_child PARTITION BY LIST (city) (
 PARTITION us VALUES IN ('seattle','san francisco','los angeles','phoenix','minneapolis',
